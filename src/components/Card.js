9243095dbb2691291f9199b0b95fcd935d913c7f
sunny <saynotta@gmail.com>
2016-11-30 11:26:18 +0800

    添加图片大小自适应

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,38 @@ import React from 'react';
 import {Card, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 class CardMy extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: '100%',
+      height: '300px'
+    };
+    window.addEventListener('resize', function () {
+      var width = this.refs.img.width;
+      this.setState(
+        {
+          height: width * 1.45
+        }
+      );
+    }.bind(this));
+  };
+  componentDidMount() {
+    var width = this.refs.img.width;
+    this.setState(
+      {
+        height: width * 1.45
+      }
+    );
+  }
   render() {
     return (
       <Card className="singleCard">
         <CardMedia
           overlay={<CardTitle title={this.props.title} />}
         >
-          <img src={this.props.poster} alt={this.props.title} />
+          <div className="img">
+            <img width={this.state.width} ref="img" height={this.state.height} src={this.props.poster} />
+          </div>
         </CardMedia>
       </Card>
     );
